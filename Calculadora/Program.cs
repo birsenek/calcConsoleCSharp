584bcc2a9366e206a9fe5055e926acf9397a3647
birsenek <birsenek@gmail.com>
2021-02-20 17:59:09 -0300

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using CalculadoraOperacoes;
+
+namespace CalculadoraProgram
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Calculadora _calculadora = new Calculadora();
+            //Operações
+            bool encerrarApp = false;
+
+            Console.WriteLine("Calculadora C#");
+            Console.WriteLine("------------------------");
+
+            while (!encerrarApp)
+            {
+                string numInput1 = null;
+                double newInput1 = 0;
+                string numInput2 = null;
+                double newInput2 = 0;
+                double result = 0;
+                string operacao = null;
+
+                while (!double.TryParse(numInput1, out newInput1))
+                {
+                    Console.WriteLine("Digite um número e pressione ENTER.");
+                    numInput1 = Console.ReadLine();
+                }
+
+                while (!double.TryParse(numInput2, out newInput2))
+                {
+                    Console.WriteLine("Digite outro número e pressione ENTER.");
+                    numInput2 = Console.ReadLine();
+                }
+
+                //Menu de Escolha
+                bool operacaoEscolhida = false;
+                string[] operacoesPossiveis = {"1", "2", "3", "4"};
+
+                Console.WriteLine("Escolha uma operação da lista:");
+                Console.WriteLine($"{operacoesPossiveis[0]} - Soma");
+                Console.WriteLine($"{operacoesPossiveis[1]} - Subtração");
+                Console.WriteLine($"{operacoesPossiveis[2]} - Multiplicação");
+                Console.WriteLine($"{operacoesPossiveis[3]} - Divisão");
+                Console.WriteLine("Opção escolhida: ");
+
+                //validação escolhas
+                while (!operacaoEscolhida)
+                {
+                    operacao = Console.ReadLine();
+                    foreach (string op in operacoesPossiveis)
+                    {
+                        if (op.Equals(operacao))
+                        {
+                            operacaoEscolhida = true;
+                            break;
+                        }
+                    }
+                    if (!operacaoEscolhida)
+                        Console.WriteLine("Operação inválida, escolha outro valor.");
+                }
+
+                try
+                {
+                    result = _calculadora.RealizarOperacao(newInput1, newInput2, operacao);
+                    Console.WriteLine("Resultado: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Errou!!" + e.Message);
+                }
+
+                //Aguardar ação do usuário para encerrar
+                bool novaOperacao = false;
+                while (!novaOperacao)
+                {
+                    Console.Write("Deseja Realizar outra operação? Pressione S para Sim ou N para Não. ");
+                    string finalizarCalculadora = Console.ReadLine();
+                    if (finalizarCalculadora == "n")
+                    {
+                        encerrarApp = true;
+                        break;
+                    }
+                    else if (finalizarCalculadora == "s")
+                    {
+                        Console.WriteLine("Reiniciando variáveis. \n");
+                        novaOperacao = true;
+                    }
+                }
+            }
+            Console.WriteLine("Encerrando.");
+
+            _calculadora.Finish();
+        }
+    }
+}
---
+++ CaluladoraLibrary/CalculadoraLibrary.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculadoraOperacoes
+{
+    public class Calculadora
+    {
+        JsonWriter writer;
+        public Calculadora()
+        {
+           //gerar logs txt e json.
+            StreamWriter logFileTxt = File.CreateText("calculadora.log");
+            StreamWriter logFileJson = File.CreateText("calculadora.json");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFileTxt));
+            Trace.AutoFlush = true;
+            writer = new JsonTextWriter(logFileJson);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+            Trace.WriteLine("Carregando o Log da Calculadora");
+            Trace.WriteLine(string.Format("Iniciado {0}", System.DateTime.Now.ToString()));
+        }
+        public double RealizarOperacao(double num1, double num2, string operacao)
+        {
+            double result = double.NaN;
+
+            //escrever json
+            writer.WriteStartObject();
+            writer.WritePropertyName("Valor1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Valor2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operacao");
+           
+            switch (operacao)
+            {
+                case "1":
+                    result = num1 + num2;
+                    Trace.WriteLine(string.Format("{0} + {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Soma");
+                    break;
+                case "2":
+                    result = num1 - num2;
+                    Trace.WriteLine(string.Format("{0} - {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Subtracao");
+                    break;
+                case "3":
+                    result = num1 * num2;
+                    Trace.WriteLine(string.Format("{0} * {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Multiplicacao");
+                    break;
+                case "4":
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("Impossível dividir por 0. Escolha outro valor como divisor");
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                    }
+
+                    result = num1 / num2;
+                    Trace.WriteLine(string.Format("{0} / {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Divisao");
+                    writer.WritePropertyName("NovoValor2");
+                    writer.WriteValue(num2);
+                    break;
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Resultado");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        //encerrar Json
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+
+}
